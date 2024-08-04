96572e4a2310f5e8efa735ca9d2870ce50e01ee1
Ubuntu <yutong.jiang2@ingka.ikea.com>
2024-08-04 19:06:04 +0000

    update decoders and postprocessor in pretrain_tokenizer

--- tokenizer/pretrain_tokenizer.py
+++ tokenizer/pretrain_tokenizer.py
@@ -4,8 +4,11 @@ from tokenizers import (
     pre_tokenizers,
     trainers,
     pre_tokenizers,
-    Tokenizer
+    Tokenizer,
+    processors,
+    decoders
 )
+import tokenizers.processors
 from transformers import PretrainedTokenizerFast
 
 special_tokens = ["<s>", "</s>", "<unk>"]
@@ -22,10 +25,23 @@ def training_tokenizer():
     ])
     
     # specify pre-tokenizers
+    # NFD: normalization form decomposition
+    # NFC: normalization form composition
+
     tokenizer.pre_tokenizer = pre_tokenizers.Sequence([
-        pre_tokenizers.ByteLevel(add_prefix_space=True, use_regex=True)
+        pre_tokenizers.Punctuation(),
+        pre_tokenizers.ByteLevel(add_prefix_space=True, use_regex=True),
+        pre_tokenizers.Metaspace(),
+        pre_tokenizers.Digits()
     ])
-    tokenizer.add_special_tokens(special_tokens)    
+    tokenizer.add_special_tokens(special_tokens)
+    
+    # initialize decoders which matches pre_tokenizer
+    tokenizer.decoder = decoders.ByteLevel(add_prefix = True, use_regex = True)
+    
+    # by default, the ByteLevel tokenizer might include whitespaces in the produced tokens
+    # use post processor to trim the output
+    tokenizer.post_processor = processors.ByteLevel()
     
     trainer = trainers.BpeTrainer(vocab_size = vocab_size, min_frequency = 50, show_progress = True, special_tokens = special_tokens)
     tokenizer.train_from_iterator(trainer, ["data/train.txt"])
